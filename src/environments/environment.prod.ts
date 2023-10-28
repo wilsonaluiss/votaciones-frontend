494003408df70f5c143d06bcb4e41342c9414ea1
awialluis <wluis@miumg.edu.gt>
2023-10-28 11:05:08 -0600

    firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  //BASE_URL_VOTACIONES: 'http://localhost:8080',
+  BASE_URL_VOTACIONES: 'https://votaciones-usuario-48c2881afa24.herokuapp.com',
+  //BASE_URL_CATALOGOS: 'http://localhost:8081',
+  BASE_URL_CATALOGOS: 'https://votaciones-catalogos-34d31cac6f4f.herokuapp.com',
+  BASE_URL_VOTO: 'https://votaciones-usuario-48c2881afa24.herokuapp.com/',
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,12 +3,12 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false,
+  production: true,
   //BASE_URL_VOTACIONES: 'http://localhost:8080',
   BASE_URL_VOTACIONES: 'https://votaciones-usuario-48c2881afa24.herokuapp.com',
   //BASE_URL_CATALOGOS: 'http://localhost:8081',
   BASE_URL_CATALOGOS: 'https://votaciones-catalogos-34d31cac6f4f.herokuapp.com',
-  BASE_URL_VOTO: 'http://localhost:8080',
+  BASE_URL_VOTO: 'https://votaciones-usuario-48c2881afa24.herokuapp.com/',
 };
 
 /*
